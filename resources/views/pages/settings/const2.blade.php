72a72b25a6ea3b81ba7e1f4d88d1717af1d5f131
Yusuke Shibata <yusuke.shibata@the-ai.co.jp>
2020-03-04 20:02:49 +0900

    行削除実装。
    行がなくなったら一緒にcardを削除するfunctionも必要だな。

--- resources/views/pages/settings/const2.blade.php
+++ resources/views/pages/settings/const2.blade.php
@@ -74,25 +74,26 @@
                             </div>
                             <span class="col-md-1 form-group">
                                 <a href="" class="" data-toggle="modal">
-                                    <i id="{{ $card_name }}-{{ $card_row_cnt }}-delete-btn"
-                                       class="btn material-icons delete-icon-circle p-0 delete-item"
+                                    <i id="{{ $card_name }}-row-{{ $card_row_cnt }}-delete-btn"
+                                       class="btn material-icons p-0 delete-item"
+                                       row="{{ $card_row_cnt }}"
                                     >remove_circle</i>
                                 </a>
                                 <a href="" class="" data-toggle="modal">
-                                    <i id="{{ $card_name }}-{{ $card_row_cnt }}-edit-btn"
-                                       class="btn material-icons edit-icon-circle p-0 edit-item"
+                                    <i id="{{ $card_name }}-row-{{ $card_row_cnt }}-edit-btn"
+                                       class="btn material-icons p-0 edit-item"
                                     >create</i>
                                 </a>
                                 <a class="" data-toggle="modal">
-                                    <i id="{{ $card_name }}-{{ $card_row_cnt }}-cancel-btn"
-                                       class="btn material-icons cancel-icon-circle p-0 text-danger"
+                                    <i id="{{ $card_name }}-row-{{ $card_row_cnt }}-cancel-btn"
+                                       class="btn material-icons p-0 text-danger"
                                        style="display: none;">
                                         cancel
                                     </i>
                                 </a>
                                 <a class="" data-toggle="modal">
                                     <i id="{{ $card_name }}-{{ $card_row_cnt }}-save-btn"
-                                       class="btn material-icons save-icon-circle p-0 text-success"
+                                       class="btn material-icons p-0 text-success"
                                        style="display: none;">
                                         check_circle
                                     </i>
@@ -123,204 +124,11 @@
             ?>
     @endforeach
 
-                            {{--
-                        <div class="row">
-                            <span class="col-md-1 form-group">
-                            </span>
-                            <div class="col-md-2 form-group text-right">
-                                const-2
-                            </div>
-                            <div class="col-md-8 form-group">
-                                <input type="text" placeholder="value" class="form-control">
-                            </div>
-                            <span class="col-md-1 form-group">
-                                <a href="" class="" data-toggle="modal">
-                                    <i class="btn material-icons delete-icon-circle p-0 delete-item">remove_circle</i>
-                                </a>
-                            </span>
-                        </div>
-                    </div>
---}}
-
-    {{--
-        <div class="card">
-            <div class="card-header" role="tab" id="heading1">
-                <h5 class="mb-0">
-                    <a class="text-body d-block p-3 m-n3" data-toggle="collapse"
-                       href="#collapse2" role="button" aria-expanded="false" aria-controls="collapse2">
-                        database
-                    </a>
-                </h5>
-            </div><!-- /.card-header -->
-            <div id="collapse2" class="collapse show" role="tabpanel"
-                 aria-labelledby="heading1" data-parent="#accordion">
-                <div class="card-body">
-                    <div id="const-database">
-                        <div id="database-const-1" class="row">
-                            <span class="col-md-1 form-group">
-                            </span>
-                            <div class="col-md-2 form-group text-right">
-                                const-1
-                            </div>
-                            <div class="col-md-8 form-group">
-                                <input type="text" placeholder="value" class="form-control">
-                            </div>
-                            <span class="col-md-1 form-group">
-                                <a href="" class="" data-toggle="modal">
-                                    <i id="database-const-1-rm-btn" class="btn material-icons delete-icon-circle p-0 delete-item">remove_circle</i>
-                                </a>
-                            </span>
-                        </div>
-                        <div class="row">
-                            <span class="col-md-1 form-group">
-                            </span>
-                            <div class="col-md-2 form-group text-right">
-                                const-2
-                            </div>
-                            <div class="col-md-8 form-group">
-                                <input type="text" placeholder="value" class="form-control">
-                            </div>
-                            <span class="col-md-1 form-group">
-                                <a href="" class="" data-toggle="modal">
-                                    <i class="btn material-icons delete-icon-circle p-0 delete-item">remove_circle</i>
-                                </a>
-                            </span>
-                        </div>
-                    </div>
-                    <div class="row" rowtype="newItem">
-                        <div class="col-md-2 form-group text-right">
-
-                        </div>
-                        <span class="col-md-10 form-group">
-                                <button id="add-items" name="database"
-                                        class="btn btn-info float-left add-icon-circle add-item"
-                                        data-toggle="modal">
-                                    <i class="material-icons add-icon-circle">add_circle</i>
-                                    <span class="">New</span>
-                                </button>
-                        </span>
-                    </div>
-                </div><!-- /.card-body -->
-            </div><!-- /.collapse -->
-        </div><!-- /.card -->
-    --}}
-{{--
-        <div class="card">
-            <div class="card-header" role="tab" id="heading2">
-                <h5 class="mb-0">
-                    <a class="collapsed text-body d-block p-3 m-n3" data-toggle="collapse"
-                       href="#collapse3" role="button" aria-expanded="false" aria-controls="collapse3">
-                        application
-                    </a>
-                </h5>
-            </div><!-- /.card-header -->
-            <div id="collapse3" class="collapse" role="tabpanel"
-                 aria-labelledby="heading2" data-parent="#accordion">
-                <div class="card-body">
-                    <div id="const-application">
-                        <div id="application-const-1" class="row">
-                            <span class="col-md-1 form-group">
-                            </span>
-                            <div class="col-md-2 form-group text-right">
-                                const-1
-                            </div>
-                            <div class="col-md-8 form-group">
-                                <input type="text" placeholder="value" class="form-control">
-                            </div>
-                            <span class="col-md-1 form-group">
-                                <a href="" class="" data-toggle="modal">
-                                    <i id="application-const-1-rm-btn" class="btn material-icons delete-icon-circle p-0 delete-item">remove_circle</i>
-                                </a>
-                            </span>
-                        </div>
-                        <div class="row" rowtype="newItem">
-                            <div class="col-md-2 form-group text-right">
-
-                            </div>
-                            <span class="col-md-10 form-group">
-                                <button id="add-items" name="database"
-                                        class="btn btn-info float-left add-icon-circle add-item"
-                                        data-toggle="modal">
-                                    <i class="material-icons add-icon-circle">add_circle</i>
-                                    <span class="">New</span>
-                                    </button>
-                            </span>
-                        </div>
-                    </div>
-                </div><!-- /.card-body -->
-            </div><!-- /.collapse -->
-        </div><!-- /.card -->
-        <div class="card">
-            <div class="card-header" role="tab" id="heading3">
-                <h5 class="mb-0">
-                    <a class="collapsed text-body d-block p-3 m-n3" data-toggle="collapse"
-                       href="#collapse4" role="button" aria-expanded="false" aria-controls="collapse4">
-                        aws
-                    </a>
-                </h5>
-            </div><!-- /.card-header -->
-            <div id="collapse4" class="collapse" role="tabpanel"
-                 aria-labelledby="heading3" data-parent="#accordion">
-                <div class="card-body">
-                    アイテム3のコンテンツ
-                </div><!-- /.card-body -->
-            </div><!-- /.collapse -->
-        </div><!-- /.card -->
-        <div class="card">
-            <div class="card-header" role="tab" id="heading4">
-                <h5 class="mb-0">
-                    <a class="collapsed text-body d-block p-3 m-n3" data-toggle="collapse"
-                       href="#collapse5" role="button" aria-expanded="false" aria-controls="collapse5">
-                        pusher
-                    </a>
-                </h5>
-            </div><!-- /.card-header -->
-            <div id="collapse5" class="collapse" role="tabpanel"
-                 aria-labelledby="heading4" data-parent="#accordion">
-                <div class="card-body">
-                    アイテム3のコンテンツ
-                </div><!-- /.card-body -->
-            </div><!-- /.collapse -->
-        </div><!-- /.card -->
-        <div class="card">
-            <div class="card-header" role="tab" id="heading5">
-                <h5 class="mb-0">
-                    <a class="collapsed text-body d-block p-3 m-n3" data-toggle="collapse"
-                       href="#collapse6" role="button" aria-expanded="false" aria-controls="collapse6">
-                        timezone
-                    </a>
-                </h5>
-            </div><!-- /.card-header -->
-            <div id="collapse6" class="collapse" role="tabpanel"
-                 aria-labelledby="heading5" data-parent="#accordion">
-                <div class="card-body">
-                    アイテム3のコンテンツ
-                </div><!-- /.card-body -->
-            </div><!-- /.collapse -->
-        </div><!-- /.card -->
-        <div class="card">
-            <div class="card-header" role="tab" id="heading6">
-                <h5 class="mb-0">
-                    <a class="collapsed text-body d-block p-3 m-n3" data-toggle="collapse"
-                       href="#collapse7" role="button" aria-expanded="false" aria-controls="collapse7">
-                        theme
-                    </a>
-                </h5>
-            </div><!-- /.card-header -->
-            <div id="collapse7" class="collapse" role="tabpanel"
-                 aria-labelledby="heading6" data-parent="#accordion">
-                <div class="card-body">
-                    アイテム3のコンテンツ
-                </div><!-- /.card-body -->
-                <div class="card-body">
-                    アイテム3のコンテンツ2
-                </div><!-- /.card-body -->
-            </div><!-- /.collapse -->
-        </div><!-- /.card -->
-end cards--}}
-    </div><!-- /#accordion -->
+   </div><!-- /#accordion -->
 </div>
 @endsection{{-- content --}}
+
+
 @section('page-foot')
     {{--
         <script src="{{ asset('assets/js/page_settings_const.js') }}"></script>
@@ -328,7 +136,20 @@ end cards--}}
     <script>
         (function($) {
 
-            /* 項目の移動設定 */
+            /* ################################
+            行の削除
+            ################################ */
+            $(document).on("click", ".delete-item", function () {
+                $("#"+$(this)[0]['id'].replace("-delete-btn", "")).remove();
+                /*
+                var targetIdObj = $("#"+$(this)[0]['id'].replace("-delete-btn", ""));
+                $(targetIdObj).remove();
+                */
+            });
+
+            /* ################################
+            項目の移動設定
+            ################################ */
             $('.{{ $plugin_name }}-rows').sortable();
 
             /*
